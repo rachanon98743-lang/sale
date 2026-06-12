88afc8443bdc1ef54495d16eb04eb5110ac2c064
Rachanon Jantarapakdeewong <workrachanon@gmail.com>
2026-06-12 14:40:59 +0700

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2151952263" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "306319796" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
